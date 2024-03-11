049335babe12013fc65178ea2f39ff9c46a3ada4
jawidh-muhammadh <jawidhmuhammadh@gmail.com>
2024-03-11 12:09:06 +0530

    first changes

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(3001, () => {
+  console.log("server is up and running");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hi this is a hit");
+});
+
+app.post("/postme", (req, res) => {
+  console.log("got a hit on postme");
+  res.send("hello this is post");
+});
